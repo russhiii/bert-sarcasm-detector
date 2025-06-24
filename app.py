290963ecf1868020ad29d2975dc7a75218297c25
russhiii <rushikhelage2004@gmail.com>
2025-06-25 00:52:13 +0530

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import gdown
 
 app = Flask(__name__)
 
-# === Step 1: Define Drive file IDs ===
 file_links = {
     "config.json": "1Y8zrSKj1j2VfZEX04zDRuKV6ZmQ4f1DI",
     "model.safetensors": "1-hUHZXABjQiQS-7wgskb9DlpeZRUttMK",
@@ -18,15 +17,12 @@ file_links = {
 model_dir = "saved_model"
 os.makedirs(model_dir, exist_ok=True)
 
-# === Step 2: Download files if missing ===
 for filename, file_id in file_links.items():
     file_path = os.path.join(model_dir, filename)
     if not os.path.exists(file_path):
-        print(f"Downloading {filename}...")
         url = f"https://drive.google.com/uc?id={file_id}"
         gdown.download(url, file_path, quiet=False)
 
-# === Step 3: Load model and tokenizer ===
 tokenizer = BertTokenizer.from_pretrained(model_dir)
 model = BertForSequenceClassification.from_pretrained(model_dir)
 model.eval()
